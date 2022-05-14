8a2f50fd3a0b73f66becd9351cbc0cb36c884c9a
Jai Dhaliwal <91633223+realjaidhaliwal@users.noreply.github.com>
2022-05-14 11:08:28 -0700

    added navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
   <v-app id="inspire">
     <v-navigation-drawer
-      v-model="drawer"
+      v-model = "drawer"
       app
     >
-      <!--  -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Boost Productivity
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app>
